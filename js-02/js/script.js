f11fa07d00a9d6569461ba428c73c8402735e8f0
Jean Paul Ferreira <jeanpaul@jepafe.net>
2022-09-27 12:17:08 -0300

    JS - 02

---
+++ js-02/js/script.js
@@ -0,0 +1,104 @@
+let num1 = 10;
+let num2 = 20;
+
+let resultado;
+
+// Operadores aritméticos
+
+// resultado = num1 + num2;
+// resultado = num1 - num2;
+// resultado = num1 * num2;
+// resultado = num2 / num1;
+// resultado = 9 % 2;
+
+// resultado = num1;
+// resultado = resultado + 2;
+// resultado += num2;
+
+// resultado = num1 + num2;
+
+// resultado = 2;
+// resultado **= 3;
+
+// Operadores comparación
+
+// resultado = "10" !== '10';
+
+// num1 = num1 + 1;
+// num1 += 1;
+// ++num1;
+
+// resultado = ++num1;
+
+num1 = 12;
+num2 = 15;
+let num3 = 22;
+
+// resultado = num1 < 12 && num3 >= 20;
+// resultado = num1 < 12 || num3 >= 20;
+
+// resultado = !false;
+
+// console.log(!num1 % 2);
+
+// if (num2 % 2 == 0) {
+// // if (num1 % 2 != 1) {
+// // if (!(num1 % 2)) {
+//     console.log('Es par');
+// } else {
+//     console.log('Es impar');
+// }
+
+// if (num3 < num2) {
+//     console.log('Se ejecuto el if');
+// }
+
+// console.log(resultado);
+
+
+// let num4 = 0;
+
+// if (num4 >= 1) {
+//     console.log('Es un numero positivo');
+// } else if (num4 == 0) {
+//     console.log('Es un numero cero');
+// } else {
+//     console.log('Es un numero negativo');
+// }
+
+// for (let i = 0; i < 3; i++) { // i = 3
+//     console.log('La variable i es igual a: ' + i);
+// }
+
+// let i = 0;
+
+// while (i < 3) {
+//     console.log('La variable i es igual a: ' + i);
+
+//     i++;
+// }
+
+// let i = 3;
+
+// do {
+//     console.log('La variable i es igual a: ' + i);
+
+//     i++;
+// } while (i < 3);
+
+let semaforo = 'amarillo';
+
+switch (semaforo) {
+    case 'verde':
+        console.log('Cruzo');
+        break;
+    case 'amarillo':
+        console.log('Precaución');
+        break;
+    case 'rojo':
+        console.log('Freno'); 
+        break;
+    default:
+        console.log('No se encontró opción');
+        break;
+}
